73ee1745ec77b4fdf44d84844f8c3525abb285fc
larueagent <larueagent@gmail.com>
2026-03-06 14:13:21 -0800

    feat: add PostHog reverse proxy rewrites

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
+    ];
+  },
+  // Required to allow the /ingest/decide endpoint to work correctly
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
